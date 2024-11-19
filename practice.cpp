8429df6720ff3c9b0aaac0b03b0a41e20357f6f7
tanishabamne <tanisha.bamne1980@gmail.com>
2024-11-19 09:45:58 -0800

    updated

---
+++ practice.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+// creat an array
+int arr[5];
+int size= sizeof(arr)/sizeof(arr[0]);
+//input
+cout<<"Enter five numbers in array:"<<endl;
+for(int i=0; i<size;i++)
+{
+    cin>>arr[5];
+
+}
+// read data from array
+for(i=0; i<size; i++)
+{
+    cout<<arr[5]<<\t<<endl;
+}
+//how to search the record
+int data,n;
+cout<<"Enter element you want to search"<<endl;
+cin>>data;
+for(int i=0;i)
+
+
+}
